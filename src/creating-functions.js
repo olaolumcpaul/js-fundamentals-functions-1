7ce5de78f2110ab99d364903b77cd50d3b02287b
Olaoluwa Oyebode <olaolumcpaul@gmail.com>
2024-04-27 23:47:15 +0100

    completed exercise

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -1,4 +1,5 @@
 // 1. Define a function that takes a number and increments it by 1.
+
 //
 // Example Input and Output:
 //
@@ -7,6 +8,11 @@
 // 2      | 3
 //
 // TODO: write code below
+function increment(num) {
+  return num + 1
+}
+console.log(increment(1))
+console.log(increment(2))
 
 // 2. Define a function that capitalises any string
 
@@ -18,6 +24,12 @@
 // Hello | Hello
 
 // TODO: write code below
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+}
+console.log('Hello')
+console.log(capitalize('world'))
+console.log(capitalize('Hello'))
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -29,6 +41,11 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function greet(name) {
+  return `Hi, ${capitalize(name)} :)`
+}
+console.log(greet('edward'))
+console.log(greet('Aiyana'))
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -41,6 +58,19 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function countStrings(array) {
+  let count = 0
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'string') {
+      count++
+    }
+  }
+  return count
+}
+console.log(countStrings(['edward']))
+console.log(countStrings(['edward', 'aiyana']))
+console.log(countStrings([1]))
+console.log(countStrings([1, 'edward']))
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -52,12 +82,21 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+function addProperty(object) {
+  if (!object.edward) {
+    object.edward = 'amazing'
+  }
+  return object
+}
+console.log(addProperty({}))
+console.log(addProperty({ aiyana: 'fantastic' }))
+console.log(addProperty({ edward: 'fabulous' }))
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: increment, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalize, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: greet, // etc
+  d: countStrings,
+  e: addProperty
 }
--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,16 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function range(lower, upper) {
+  const array = []
+  for (let i = lower; i <= upper; i++) {
+    array.push(i)
+  }
+  return array
+}
+console.log(range(1, 3))
+console.log(range(10, 13))
+console.log(range(-1, 1))
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +31,12 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function exclamations(string, number) {
+  return string.toUpperCase() + '!'.repeat(number)
+}
+console.log(exclamations('disaster', 5))
+console.log(exclamations('error', 10))
+console.log(exclamations('success', 0))
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -33,10 +49,23 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+function addTime(time, minutes) {
+  const [hour, minute] = time.split(':')
+  const newMinute = (parseInt(minute) + minutes) % 60
+  const newHour =
+    (parseInt(hour) + Math.floor((parseInt(minute) + minutes) / 60)) % 24
+  return `${newHour}:${newMinute.toString().padStart(2, '0')}`
+}
+
+console.log(addTime('7:50', 4))
+console.log(addTime('7:50', 72))
+console.log(addTime('11:50', 20))
+console.log(addTime('12:50', 120))
+console.log(addTime('23:50', 30))
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: range, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: exclamations, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: addTime // etc
 }
